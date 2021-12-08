d34fedaeb310d6a215491fb85b98e7e0bad2e15f
P-0585\user <iamkang02@i-screamedu.co.kr>
2021-12-08 18:11:49 +0900

    로그인, 로그아웃 기능 / 인증된 계정만 게시글 등록 권한 부여

--- src/main/java/com/spring/book/hellospring/web/IndexController.java
+++ src/main/java/com/spring/book/hellospring/web/IndexController.java
@@ -1,5 +1,8 @@
 package com.spring.book.hellospring.web;
 
+import com.spring.book.hellospring.config.auth.LoginUser;
+import com.spring.book.hellospring.config.auth.dto.SessionUser;
+import com.spring.book.hellospring.domain.user.User;
 import com.spring.book.hellospring.service.PostsService;
 import com.spring.book.hellospring.web.dao.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +11,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userNames", user.getName());
+        }
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        System.out.println(model);
+
         return "index";
     }
 
